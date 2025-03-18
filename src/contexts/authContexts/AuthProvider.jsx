af2bf96f4bc95de279f511f6a58f64ba15c0993e
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-03-18 16:35:06 +0600

    finished the use auth and create auth context is finished

--- src/contexts/authContexts/AuthProvider.jsx
+++ src/contexts/authContexts/AuthProvider.jsx
@@ -1,13 +1,41 @@
-import { createContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../firebase/firebase';
 
-const authContext = createContext();
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const [userLoggedIn, setUserLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
 
-  return <>{children}</>;
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, initializeUser);
+    return unsubscribe;
+  }, []);
+
+  async function initializeUser(user) {
+    if (user) {
+      setCurrentUser({ ...user });
+      setUserLoggedIn(true);
+    } else {
+      setCurrentUser(null);
+      setUserLoggedIn(false);
+    }
+    setLoading(false);
+  }
+
+  const value = {
+    currentUser,
+    userLoggedIn,
+    loading,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
-export default index;
+export default AuthProvider;
