a29a022a5557d5a8e7eb4f746c95ffa6b76262ad
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-03-18 13:18:56 +0600

    added current user, suerloggedin and laoding variable to the authProvider.jsx

--- src/contexts/authContexts/AuthProvider.jsx
+++ src/contexts/authContexts/AuthProvider.jsx
@@ -1,7 +1,12 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const authContext = createContext();
+
 export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   return <>{children}</>;
 };
 
